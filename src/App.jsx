f3f68c8998e786998832eb84cea570c6510fb3dd
Sanjay Negi <sanjaynegi@gmail.com>
2026-03-01 08:46:21 +0530

    checking its not ignored

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>CI/CD using Github Actions</h1>
-      <h2>For beginners v2</h2>
+      <h2>For beginners v3</h2>
       <h2>With 2 projects</h2>
     </>
   )
