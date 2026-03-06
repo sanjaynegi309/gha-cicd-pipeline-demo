f9236bbcad0d88b3528f88abd586bc064daf30e4
Sanjay Negi <sanjaynegi@gmail.com>
2026-03-07 04:44:21 +0530

    added a new feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h1>CI/CD using Github Actions</h1>
       <h2>For beginners v3</h2>
-      <h2>With 2 projects</h2>
+      <h2>With 2  project branches</h2>
     </>
   )
 }
